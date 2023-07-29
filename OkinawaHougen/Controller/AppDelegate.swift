eb8fbfb2396143236b57424a3b28553959f13ee4
Gushiken <gusiken0604@gmail.com>
2023-07-30 01:56:10 +0900

    データベース最初の10個を更新、次は文の配置を変えたい

--- OkinawaHougen/Controller/AppDelegate.swift
+++ OkinawaHougen/Controller/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         print(Realm.Configuration.defaultConfiguration.fileURL!)
-        
+        print("shift+command+gでパスを貼り付け")
         hougenDB.addHougenDB()
 
         let defaultRealmPath = Realm.Configuration.defaultConfiguration.fileURL!
